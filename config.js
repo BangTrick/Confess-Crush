e445a6aa0d440edd5c6847144197f483311aa67f
BangTrick <91129067+BangTrick@users.noreply.github.com>
2021-10-21 20:34:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
     introTitle: 'Xin chào người ae của tôi',
-    introDesc: `Trái đất luôn tồn tại 
-    những người ae tốt,bạn có
-    thể bao net tôi ko
-    nếu ko thì pp người anh em`,
+    introDesc: `Web này để thử xem 
+    bạn có xem tôi là anh
+    em của bạn 
+    hay không`,
     btnIntro: '^^ được^^',
-    title: 'tôi hay chơi ở quán net 10k/1h🥰',
-    desc: 'Là người anh em tốt thì chúng ta nên bao 5h nhé :3 ',
-    btnYes: 'Oke nè',
-    btnNo: 'không',
-    question: 'Trên thế giới hơn 7 tỉ người mà tôi chỉ coi bạn là người ae duy nhất, hãy bao net tôi nhé :33',
+    title: 'bạn có xem tôi là anh em của bạn không🥰',
+    desc: 'hãy chọn 1 trong 2 đi',
+    btnYes: ' anh em cc',
+    btnNo: 'm là th anh em tốt nhất t từng gặp',
+    question: ' đúng là anh em tốt, bao net tôi nha',
     btnReply: 'Gửi cho bạn <3',
     reply: 'được chứ sao ko, bạn ko cần trả tiền net đâu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi net nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me/a.b0220' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/quangdat.tran.7330' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
