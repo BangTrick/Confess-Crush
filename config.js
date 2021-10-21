5b4767129a375365b816085f1cc642cfc8309dbd
BangTrick <91129067+BangTrick@users.noreply.github.com>
2021-10-21 12:34:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    introTitle: 'Xin chào người ae của tôi',
+    introDesc: `Trái đất luôn tồn tại 
+    những người ae tốt,bạn có
+    thể bao net tôi ko
+    nếu ko thì pp người anh em`,
+    btnIntro: '^^ được^^',
+    title: 'tôi hay chơi ở quán net 10k/1h🥰',
+    desc: 'Là người anh em tốt thì chúng ta nên bao 5h nhé :3 ',
+    btnYes: 'Oke nè',
+    btnNo: 'được lun :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà tôi chỉ coi bạn là người ae duy nhất, hãy bao net tôi nhé :33',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'được chứ sao ko, bạn ko cần trả tiền net đâu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi net nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/a.b0220' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
